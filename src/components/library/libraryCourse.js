1c0c3f29bbda67dfc41bd402bae23876cfb5b9c6
Jared Grant <jared.grant@canyonsdistrict.org>
2018-07-30 08:51:33 -0600

    font awesome and custom icons

--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
+import Icon from '../icon';
+
 class LibraryCourse extends Component {
     render() {
         return (
             <div className="library-course">
                 <label calssName="library-course__title">Problem Solving</label>
+                { Icon('fas fa-check', 'library-course__icon')}
+                { Icon('fas fa-plus-circle', 'library-course__action')}
                 <div className="library-course__description">
                     <label>Course Description</label>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum blandit nibh, et venenatis quam tempor vel. Nam turpis enim, varius at arcu non, ultricies consectetur enim. </p>
